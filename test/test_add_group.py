f2ad3c80ebae2e9e4c5d6f006355d3df79f2b653
Ольга <Olechka56741@gmail.com>
2018-06-20 15:21:33 +0300

    Добавлено обеспечение выполнения предусловий

--- test/test_add_group.py
+++ test/test_add_group.py
@@ -3,9 +3,13 @@ from model.group import Group
 
 
 def test_add_group(app):
+    if app.group.count == 0:
+        app.group.create(Group(name="test"))
     app.group.create(Group(name="rrr", header="sss", footer="qqq"))
 
 
 def test_add_empty_group(app):
+    if app.group.count == 0:
+        app.group.create(Group(name="test"))
     app.group.create(Group(name="", header="", footer=""))
 
